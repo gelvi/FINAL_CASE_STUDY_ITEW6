ef247139fd3d395b68aed43dd10b7beeee648deb
Gelvi Ryan <rosariagelvi07@gmail.com>
2024-06-14 02:24:14 +0800

    Adding and removing unnecessary

--- HMS - API/routes/api.php
+++ HMS - API/routes/api.php
@@ -5,44 +5,41 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\PatientController;
 use App\Http\Controllers\AppointmentController;
-use App\Http\Controllers\MedicalRecordController;
-use Illuminate\Http\Request; // Add this line to import the Request class
+use App\Http\Controllers\MedicalRecordsController;
+
+Route::middleware('auth:sanctum')->get('/doctor/details', [DoctorController::class, 'details']);
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-// Route group for authenticated users
-Route::middleware('auth:sanctum')->group(function () {
-    // Route to fetch the authenticated user's data
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+// Routes for managing doctors
+Route::get('/doctors', [DoctorController::class, 'index']);
+Route::post('/newdoctors', [DoctorController::class, 'store']);
+Route::get('/doctor/{id}', [DoctorController::class, 'show']);
+Route::put('/updatedoctor/{id}', [DoctorController::class, 'update']);
+Route::delete('/deletedoctor/{id}', [DoctorController::class, 'destroy']);
+Route::get('/searchdoctor/{query}', [DoctorController::class, 'search']);
 
-    // Routes for managing doctors
-    Route::get('/doctors', [DoctorController::class, 'index']);
-    Route::post('/doctors', [DoctorController::class, 'store']);
-    Route::get('/doctors/{id}', [DoctorController::class, 'show']);
-    Route::put('/doctors/{id}', [DoctorController::class, 'update']);
-    Route::delete('/doctors/{id}', [DoctorController::class, 'destroy']);
+// Routes for managing patients 
+Route::get('/patients', [PatientController::class, 'index']);
+Route::post('/newpatients', [PatientController::class, 'store']);
+Route::get('/patient/{id}', [PatientController::class, 'show']);
+Route::put('/updatepatient/{id}', [PatientController::class, 'update']);
+Route::delete('/deletepatient/{id}', [PatientController::class, 'destroy']);
+Route::get('/searchpatient/{query}', [PatientController::class, 'search']);
 
-    // Routes for managing patients
-    Route::get('/patients', [PatientController::class, 'index']);
-    Route::post('/patients', [PatientController::class, 'store']);
-    Route::get('/patients/{id}', [PatientController::class, 'show']);
-    Route::put('/patients/{id}', [PatientController::class, 'update']);
-    Route::delete('/patients/{id}', [PatientController::class, 'destroy']);
+// Routes for managing appointments
+Route::get('/appointments', [AppointmentController::class, 'index']);
+Route::post('/newappointments', [AppointmentController::class, 'store']);
+Route::get('/appointment/{id}', [AppointmentController::class, 'show']);
+Route::put('/updateappointment/{id}', [AppointmentController::class, 'update']);
+Route::delete('/deleteappointment/{id}', [AppointmentController::class, 'destroy']);
+Route::get('/searchappointment/{query}', [AppointmentController::class, 'search']);
 
-    // Routes for managing appointments
-    Route::get('/appointments', [AppointmentController::class, 'index']);
-    Route::post('/appointments', [AppointmentController::class, 'store']);
-    Route::get('/appointments/{id}', [AppointmentController::class, 'show']);
-    Route::put('/appointments/{id}', [AppointmentController::class, 'update']);
-    Route::delete('/appointments/{id}', [AppointmentController::class, 'destroy']);
-
-    // Routes for managing medical records
-    Route::get('/medical-records', [MedicalRecordController::class, 'index']);
-    Route::post('/medical-records', [MedicalRecordController::class, 'store']);
-    Route::get('/medical-records/{id}', [MedicalRecordController::class, 'show']);
-    Route::put('/medical-records/{id}', [MedicalRecordController::class, 'update']);
-    Route::delete('/medical-records/{id}', [MedicalRecordController::class, 'destroy']);
-});
+// Routes for managing medical records
+Route::get('/medicalrecords', [MedicalRecordsController::class, 'index']);
+Route::post('/newmedicalrecords', [MedicalRecordsController::class, 'store']);
+Route::get('/medicalrecord/{id}', [MedicalRecordsController::class, 'show']);
+Route::put('/updatemedicalrecord/{id}', [MedicalRecordsController::class, 'update']);
+Route::delete('/deletemedicalrecords/{id}', [MedicalRecordsController::class, 'destroy']);
+Route::get('/searchmedicalrecord/{query}', [MedicalRecordsController::class, 'search']);
